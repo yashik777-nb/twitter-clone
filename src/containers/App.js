27b11e581184772808c0fd9f540c11a408b71447
Yash <yaswanth.iddibani@gmail.com>
2021-07-05 11:32:56 +0100

    Folder Restructure

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Sidebar from "./Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Twitter Clone</h1>
+
+      {/* Side Bar */}
+      <Sidebar />
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Sidebar.module.css";
+
+import TwitterIcon from "@material-ui/icons/Twitter";
+// import HomeIcon from "@material-ui/icons/Home";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+      {/* Sidebaroption componet */}
+      {/* Sidebaroption componet */}
+      {/* Sidebaroption componet */}
+      {/* Sidebaroption componet */}
+      {/* Sidebaroption componet */}
+      {/* Sidebaroption componet */}
+      {/* button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
